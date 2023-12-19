fc768216446346d2fb13882f798fb7d145de0d16
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2023-12-19 17:13:15 +0100

    base configuration

---
+++ routers/client.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const clientRouter = express.Router();
+
+clientRouter.get('/', (req, res) => {
+  res.send('works');
+});
+
+module.exports = {
+  clientRouter,
+}
